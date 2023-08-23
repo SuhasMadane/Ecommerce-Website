e5c91315b2ab3d4d67b02bbc34ac8c923e025e85
Suhas Madane <madanesuhas9@gmail.com>
2023-08-23 09:22:47 +0530

    ProductReviewCard Created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./customer/Pages/HomePage/HomePage";
 import ProductsPage from "./customer/Pages/ProductsPage";
+import ProductDetails from "./customer/Components/ProductDetails/ProductDetails";
+import ProductDetailsPage from "./customer/Pages/ProductDetailsPage";
 
 function App() {
   return (
     <>
       {/* <HomePage /> */}
-      <ProductsPage />
+      {/* <ProductsPage /> */}
+      <ProductDetailsPage />
     </>
   );
 }
---
+++ src/customer/Pages/ProductDetailsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductDetails from "../Components/ProductDetails/ProductDetails";
+import Footer from "../Components/Footer/Footer";
+
+const ProductDetailsPage = () => {
+  return (
+    <>
+      <ProductDetails /> <Footer />
+    </>
+  );
+};
+
+export default ProductDetailsPage;
